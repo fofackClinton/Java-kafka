3eac6b8db939902e62eaa99454c441fd99964137
fofackClinton <fofackclinton23@icloud.com>
2026-02-10 17:59:41 +0100

    lock et unlock

--- demo/src/main/java/com/example/threads/TaskCalculator.java
+++ demo/src/main/java/com/example/threads/TaskCalculator.java
@@ -1,10 +1,18 @@
 package com.example.threads;
 
+import java.util.concurrent.locks.Lock;
+
 public class TaskCalculator {
     private  int total =0;
+    private Lock lock;
 
     public void increment(){
-        total++;
+        lock.lock();
+        try {
+            total++;
+        } finally {
+            lock.unlock();
+        }
     }
 
     public int getTotal() {
